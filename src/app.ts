058e1f42b9a2188a65e4b144dfcdfff8b589c8cd
majharul <majharuljony163@gmail.com>
2023-09-17 19:33:32 +0600

    [Done] API gateway 2 part

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,14 @@ import routes from './app/routes';
 
 const app: Application = express();
 
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use('/api/v1', routes);
+app.use('/api/v1', routes);
+
 
 app.use(globalExceptionHandler);
 
--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -1,18 +1,18 @@
-import { createClient } from 'redis';
-import logger from './logger';
-import config from '../config';
+import { createClient } from "redis";
+import logger from "./logger";
+import config from "../config";
 
 let redisClient = createClient({
-  url: config.redis.url
+    url: config.redis.url
 });
 
 redisClient.on('error', (err) => logger.error('RedisError', err));
 redisClient.on('connect', (err) => logger.info('Redis connected'));
 
 const connect = async (): Promise<void> => {
-  await redisClient.connect();
-};
+    await redisClient.connect()
+}
 
 export const RedisClient = {
-  connect
-};
+    connect
+}
