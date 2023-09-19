2f7ef9da07714d1d450c5957019e7af5cdae8530
majharul <majharuljony163@gmail.com>
2023-09-19 11:39:21 +0600

    [Done] create student in auth service

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -11,23 +11,23 @@ router.get('/', AcademicSemesterController.getAllFromDB);
 router.get('/:id', AcademicSemesterController.getByIdFromDB);
 
 router.post(
-    '/',
-    validateRequest(AcademicSemesterValidation.create),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    AcademicSemesterController.insertIntoDB
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.insertIntoDB
 );
 
 router.patch(
-    '/:id',
-    validateRequest(AcademicSemesterValidation.update),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    AcademicSemesterController.updateOneIntoDB
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateOneIntoDB
 );
 
 router.delete(
-    '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    AcademicSemesterController.deleteByIdFromDB
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
 );
 
 export const academicSemesterRoutes = router;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { FileUploadHelper } from '../../../helpers/fileUploaderHelper';
 import { IUploadFile } from '../../../interfaces/file';
+import { AuthService } from '../../../shared/axios';
+import { IGenericResponse } from '../../../interfaces/common';
 
 const createStudent = async (req: Request) => {
   const file = req.file as IUploadFile;
@@ -10,7 +12,40 @@ const createStudent = async (req: Request) => {
     req.body.profileImage = uploadedImage.secure_url;
   }
 
-  console.log(req.body);
+  const { academicDepartment, academicFaculty, academicSemester } = req.body.student;
+
+  const academicDepartmentResponse = await AuthService.get(
+    `/academic-departments?syncId=${academicDepartment}`
+  );
+
+  if (academicDepartmentResponse.data && Array.isArray(academicDepartmentResponse.data)) {
+    req.body.student.academicDepartment = academicDepartmentResponse.data[0].id;
+  }
+
+  const academicFacultyResponse = await AuthService.get(
+    `/academic-faculties?syncId=${academicFaculty}`
+  );
+
+  if (academicFacultyResponse.data && Array.isArray(academicFacultyResponse.data)) {
+    req.body.student.academicFaculty = academicFacultyResponse.data[0].id;
+  }
+
+  const academicSemesterResponse = await AuthService.get(
+    `/academic-semesters?syncId=${academicSemester}`
+  );
+
+  if (academicSemesterResponse.data && Array.isArray(academicSemesterResponse.data)) {
+    req.body.student.academicSemester = academicSemesterResponse.data[0].id;
+  }
+
+  console.log('sddddd', req.body);
+  const response: IGenericResponse = await AuthService.post('/users/create-student', req.body, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+
+  return response;
 };
 
 export const UserService = {
